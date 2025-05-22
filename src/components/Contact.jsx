0c3c3957f18a8d47d2377625fbc5e421a8bb26fa
sejincp <sagela127@gmail.com>
2025-05-22 11:29:47 -0700

    Contact draft

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
 
+const contactText = [
+  {
+      link: "https://github.com/sejincp",
+      title: "Github",
+  },
+  {
+      link: "mailto:sejincp.dev@gmail.com",
+      title: "sejincp.dev@gmail.com",
+  },
+];
+
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <section id="contact">
+            <div className="contact__inner">
+                <h2 className="contact__title">Contact</h2>
+                <div className="contact__text">
+                  <div className="text">
+                      {contactText.map((contact, key) => (
+                          <div key={key}>
+                              <a
+                                  href={contact.link}
+                                  rel="noopener noreferrer"
+                                  target="_blank"
+                              >
+                                  {contact.title}
+                              </a>
+                          </div>
+                      ))}
+                  </div>
+                </div>
+            </div>
+        </section>
   )
 }
 
