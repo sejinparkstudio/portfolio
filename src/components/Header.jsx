658e7ddc233fd4a36d01b9f792fab5eba9e71e00
sejincp <sagela127@gmail.com>
2025-05-22 07:11:04 -0700

    Add header toggle

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,39 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react'
 import sjicon from './../assets/sjicon.svg';
 
+const headerNav = [
+  {
+      title: "about",
+      url: "#about",
+  },
+  {
+      title: "experience",
+      url: "#experience",
+  },
+  {
+      title: "projects",
+      url: "#projects",
+  },
+  {
+      title: "contact",
+      url: "#contact",
+  },
+];
+
 const Header = () => {
+  const [show, setShow] = useState(false);
+  
+  const toggleMenu = () => {
+    setShow((prevShow) => !prevShow);
+  }
 
   return (
     <>
       <header id="header" role="banner">
         <div className="header__inner">
             <div className="header__logo">
-                <a href="/">
-                  <img
-                    src={sjicon}
-                    width="30"
-                    height="30"
-                    alt="Sejin Logo"
-                  />
-                </a>
+              <a href="/">
+                <img
+                  src={sjicon}
+                  width="30"
+                  height="30"
+                  alt="Sejin Logo"
+                />
+              </a>
             </div>
-            <nav className="header__nav" role="navigation" aria-label="main nav">
+              <nav className={`header__nav ${show ? "show" : ""}`} role="navigation" aria-label="main nav">
                 <ul>
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#experience">Experience</a></li>
-                    <li><a href="#projects">Projects</a></li>
-                    <li><a href="#contact">contact</a></li>
+                  {headerNav.map((nav, key) => (
+                    <li key={key}>
+                      <a href={nav.url}>{nav.title}</a>
+                    </li>
+                  ))}
                 </ul>
-            </nav>
-            <div 
-                className="header__nav__mobile" 
-                id="headerToggle" 
-                aria-controls="primary-menu" 
-                aria-expanded="false" 
-                role="button" 
-                tabIndex="0"
+              </nav>
+            <div
+              className="header__nav__mobile" 
+              id="headerToggle" 
+              aria-controls="primary-menu" 
+              aria-expanded={show ? "true" : "false"} 
+              role="button" 
+              tabIndex="0"
+              onClick={toggleMenu}
             >
-                <span></span>
+              <span></span>
             </div>
         </div>
       </header>
